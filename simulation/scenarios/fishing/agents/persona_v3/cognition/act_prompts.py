919353e378c9ef23d82bfe46501fc73989ff57bd
shreymittal1000 <shreymittal1000@gmail.com>
2025-02-11 15:20:15 +0100

    Fixing the 2024

--- simulation/scenarios/fishing/agents/persona_v3/cognition/act_prompts.py
+++ simulation/scenarios/fishing/agents/persona_v3/cognition/act_prompts.py
@@ -35,7 +35,7 @@ def prompt_action_choose_amount_of_fish_to_catch(
         lm += f"\n"
         lm += f"Task: With a fishing range set between {interval[0]}-{interval[-1]}, how many tons of fish would you catch this month? "
         lm += reasoning_steps_prompt()
-        lm += ' Put the final answer after "Answer:", example Answer: N tons. You MUST end your answer in this format.'
+        lm += ' Put the final answer after "Answer:", example Answer: N tons. You MUST end your answer in this "Answer: N tons" format.'
 
     with assistant():
         lm = model.gen(
